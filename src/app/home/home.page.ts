b40c78ee7b90be15128ae8f0faa3064709b4f27e
davidrbv <davidrbv@gmail.com>
2022-01-19 09:56:40 +0100

    Primer commit. Estructura front.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  objetivo: number = 0;
+  puntuacion: number = 0;
+  intentos: number = 5;
+  tirada: number;
+  acumulado: number = 0;
 
+  constructor(public alertController: AlertController) {}
+
+  randomNumber() {
+    if (this.intentos < 1) {
+      this.presentAlertConfirm(this.acumulado);
+    } else {
+      this.objetivo = Math.round(Math.random() * (100 - 1) + 1);
+      this.intentos--;
+    }
+  }
+
+  comparar() {
+    if (this.intentos < 1) {
+      this.presentAlertConfirm(this.acumulado);
+    } else {
+      this.puntuacion = Math.abs(this.objetivo - this.tirada);
+      this.acumulado += this.puntuacion;
+      this.intentos--;
+    }
+  }
+
+  async presentAlertConfirm(n: number) {
+    const alert = await this.alertController.create({
+      header: '¡Diana!',
+      message: `Se te acabaron los intentos. Has conseguido ${this.acumulado} puntos`,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.reiniciaPartida();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  reiniciaPartida() {
+    this.objetivo = 0;
+    this.puntuacion = 0;
+    this.intentos = 5;
+    this.acumulado = 0;
+  }
 }
